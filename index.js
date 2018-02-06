380fae30b02cb383f5cc0216c0e98f21f8c6d80f
Brigitte Hofmeister <bhofmei@gmail.com>
2018-02-06 13:01:19 -0500

    fixed issue with replace not taking config options

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var templateUrlRegex = /templateUrl\s*:(\s*['"`](.*?)['"`]\s*([,}]))/gm;
 var stylesRegex = /styleUrls *:(\s*\[[^\]]*?\])/g;
 var stringRegex = /(['`"])((?:[^\\]\\\1|.)*?)\1/g;
 
-function replaceStringsWithRequires(string) {
+function replaceStringsWithRequires(string, config) {
   return string.replace(stringRegex, function (match, quote, url) {
     if(url.charAt(0) !== "." && config.baseRef){
       url = config.baseRef + '/' + url;
@@ -20,7 +20,7 @@ function replaceStringsWithRequires(string) {
 module.exports = function(source, sourcemap) {
 
   var config = {};
-  var query = loaderUtils.parseQuery(this.query);
+  var query = loaderUtils.getOptions(this);
   var styleProperty = 'styles';
   var templateProperty = 'template';
 
@@ -43,14 +43,14 @@ module.exports = function(source, sourcemap) {
                  // with: template: require('./path/to/template.html')
                  // or: templateUrl: require('./path/to/template.html')
                  // if `keepUrl` query parameter is set to true.
-                 return templateProperty + ":" + replaceStringsWithRequires(url);
+                 return templateProperty + ":" + replaceStringsWithRequires(url, config);
                })
                .replace(stylesRegex, function (match, urls) {
                  // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
                  // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
                  // or: styleUrls: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
                  // if `keepUrl` query parameter is set to true.
-                 return styleProperty + ":" + replaceStringsWithRequires(urls);
+                 return styleProperty + ":" + replaceStringsWithRequires(urls, config);
                });
 
   // Support for tests
